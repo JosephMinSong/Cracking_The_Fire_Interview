ce7be8729ec782500f64bd4b57a87678b8046a05
Joseph Song <song.josephmin@gmail.com>
2023-08-11 20:49:12 -0700

    landing page

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -20,7 +20,7 @@ export default function BottomBar() {
                             <Link
                                 href={link.route}
                                 key={link.label}
-                                className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+                                className={`bottombar_link ${isActive && 'bg-primary-500'}`}
                             >
                                 <Image
                                     src={link.imgURL}
@@ -29,8 +29,8 @@ export default function BottomBar() {
                                     height={24}
                                 />
 
-                                <p className="text-light-1 max-lg:hidden">
-                                    {link.label}
+                                <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                                    {link.label.split( /\s+./ )[0]}
                                 </p>
                             </Link>
                         </div>
