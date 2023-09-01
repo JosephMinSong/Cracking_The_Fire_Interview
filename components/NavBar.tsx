a91dcc5375956b7b13d8e93c724113afc1b01288
Joseph Song <song.josephmin@gmail.com>
2023-08-31 17:41:31 -0700

    navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,25 @@
+import { UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+
 export default function NavBar() {
     return (
-        <>
-        </>
+        <nav className="navbar-container flex justify-between items-center m-2">
+            <div className="">
+                <Image
+                    src='/company-logo.png'
+                    width={75}
+                    height={75}
+                    alt="Company logo icon"
+                />
+            </div>
+
+            <div>
+
+            </div>
+
+            <div>
+                <UserButton afterSignOutUrl="/" />
+            </div>
+        </nav>
     )
 }
