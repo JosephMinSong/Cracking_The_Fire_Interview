6aa648165d164b693b9cfaf90d4e7bf4c18551d8
Joseph Song <song.josephmin@gmail.com>
2023-08-31 17:22:38 -0700

    styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer() {
     return (
-        <div className="footer flex items-center justify-center mt-10 p-10">
+        <div className="footer flex items-center justify-center mt-10 p-10 text-center">
             <Image
                 src='/copyright.png'
                 width={30}
