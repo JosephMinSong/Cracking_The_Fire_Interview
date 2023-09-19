a92db29a93f9d4fef3b3c335c86a0c1a2fc48146
Joseph Song <song.josephmin@gmail.com>
2023-09-19 01:08:39 -0700

    profile tabs

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,7 +1,6 @@
-'use client'
-
 import Image from "next/image"
 import { currentUser } from "@clerk/nextjs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 interface Props {
     id: string,
@@ -11,7 +10,7 @@ interface Props {
     bio: string
 }
 
-export default function UserProfile( {
+export default async function UserProfile( {
     id,
     username,
     image,
@@ -19,32 +18,53 @@ export default function UserProfile( {
     bio
 }: Props ) {
 
+    const user = await currentUser();
+    if ( !user ) return null
+
     return (
-        <main className="flex flex-col justify-center items-center gap-8">
-            <div>
-                <Image
-                    src={image}
-                    width={175}
-                    height={175}
-                    alt="User profile picture"
-                    className="rounded-xl"
-                />
+        <main className="flex justify-between items-center w-full">
+            <div className="flex flex-col flex-1 justify-center items-center gap-8">
+                <div>
+                    <Image
+                        src={image}
+                        width={175}
+                        height={175}
+                        alt="User profile picture"
+                        className="rounded-xl"
+                    />
+                </div>
+                <div className="profile-div">
+                    <h1 className="font-bold">Username: </h1>
+                    <h1 className="text-2xl font-bold">{username}</h1>
+                </div>
+                <div className="profile-div">
+                    <h2 className="font-bold">Bio:</h2>
+                    <h2>{bio}</h2>
+                </div>
+                <div className="profile-div">
+                    <h2 className="font-bold">Status:</h2>
+                    <h2>{status}</h2>
+                </div>
+                <div className="profile-buttons">
+                    {user.id == id ? (
+                        <h1>Edit Profile</h1>
+                    ) : (
+                        <>
+                            <h1>Add Friend</h1>
+                            <h1>Send Message</h1>
+                        </>
+                    )}
+                </div>
             </div>
-            <div className="profile-div">
-                <h1 className="font-bold">Username: </h1>
-                <h1 className="text-2xl font-bold">{username}</h1>
-            </div>
-            <div className="profile-div">
-                <h2 className="font-bold">Bio:</h2>
-                <h2>{bio}</h2>
-            </div>
-            <div className="profile-div">
-                <h2 className="font-bold">Status:</h2>
-                <h2>{status}</h2>
-            </div>
-            <div className="profile-buttons">
-                <h1>Add Friend</h1>
-                <h1>Send Message</h1>
+            <div className="flex flex-col flex-1">
+                <Tabs defaultValue="account" className="w-[400px]">
+                    <TabsList>
+                        <TabsTrigger value="account">Account</TabsTrigger>
+                        <TabsTrigger value="password">Password</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="account">Make changes to your account here.</TabsContent>
+                    <TabsContent value="password">Change your password here.</TabsContent>
+                </Tabs>
             </div>
         </main>
     )
