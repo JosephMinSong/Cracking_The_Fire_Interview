dfe405f240ee64ca4c03f2bed7cef6352d52f843
JosephMinSong <song.josephmin@gmail.com>
2023-11-20 00:35:14 -0800

    new

--- next.config.js
+++ next.config.js
@@ -1,28 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // eslint: {
-    //     ignoreDuringBuilds: true,
-    // },
-    // images: {
-    //     remotePatterns: [
-    //         {
-    //             protocol: 'https',
-    //             hostname: "img.clerk.com"
-    //         },
-    //         {
-    //             protocol: "https",
-    //             hostname: "images.clerk.dev"
-    //         },
-    //         {
-    //             protocol: "https",
-    //             hostname: "uploadthing.com",
-    //         },
-    //         {
-    //             protocol: "https",
-    //             hostname: "placehold.co"
-    //         }
-    //     ]
-    // }
+    experimental: {
+        esmExternals: "loose", // <-- add this
+        serverComponentsExternalPackages: ["mongoose"] // <-- and this
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "img.clerk.com"
+            },
+            {
+                protocol: "https",
+                hostname: "images.clerk.dev"
+            },
+            {
+                protocol: "https",
+                hostname: "uploadthing.com",
+            },
+            {
+                protocol: "https",
+                hostname: "placehold.co"
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
